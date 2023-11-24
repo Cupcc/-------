47b633e64cd34e19ee09a548a9ec1abd72ce4767
Chris <ccyiyili@163.com>
2023-11-25 00:44:35 +0800

    create day9

---
+++ day9/28.Find the Index of the First Occurrence in a String.cpp
@@ -0,0 +1,18 @@
+/**
+ * @Date: 2023 Nov 25
+ * @Time: 00:43
+ * @Author: Chris
+ * @Desc: 28.Find the Index of the First Occurrence in a String
+ * @Link:
+ *https://leetcode.cn/problems/find-the-index-of-the-first-occurrence-in-a-string/description/
+ **/
+// kmp 算法
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+ public:
+  int strStr(string haystack, string needle) {}
+};
+int main() { return 0; }
