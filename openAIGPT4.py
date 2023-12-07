5ae8943cbf3521fe2a4692bfc2077595fcaa386e
Chris From Intel <664373438@qq.com>
2023-12-07 16:00:55 +0800

    add argparse to openAIGPT4.py file

--- openAIGPT4.py
+++ openAIGPT4.py
@@ -1,40 +1,80 @@
 from openai import OpenAI
+import argparse
 
-apiUrl = 'https://ssapi.onechat.shop/v1'
-apiKey = 'sk-j0lQuKMEF3UlB9Hu1e9aAc6d661e47D28b96E7Dd2aFc801b'
 
-# openai.api_base = apiUrl
-# openai.api_key = apiKey
-# prompt = "python3 使用request库调用chatgpt api,如何实现stream output in console"
-prompt = "翻译一下：文件描述符"
+def request_gpt(args):
+    apiUrl = 'https://ssapi.onechat.shop/v1'
+    apiKey = 'sk-j0lQuKMEF3UlB9Hu1e9aAc6d661e47D28b96E7Dd2aFc801b'
 
-model = "gpt-4-1106-preview"
+    # openai.api_base = apiUrl
+    # openai.api_key = apiKey
+    # prompt = "python3 使用request库调用chatgpt api,如何实现stream output in console"
+    prompt = args.prompt
 
-client = OpenAI(
-    api_key=apiKey,
-    base_url=apiUrl,
-)
+    model = args.model
 
-stream = client.chat.completions.create(
-    messages=[
-        {
-            "role":
-            "system",
-            "content":
-            "You act as an embedded development expert who helps me to build programs."
-        },
-        {
-            "role": "user",
-            "content": prompt
-        },
-    ],
-    model=model,
-    stream=True,
-)
-print(next(stream))
-if stream:
-    for chunk in stream:
-        if chunk.choices[0].delta.content is not None:
-            print(chunk.choices[0].delta.content, end="", flush=True)
-else:
-    print("Failed to request.")
+    client = OpenAI(
+        api_key=apiKey,
+        base_url=apiUrl,
+    )
+
+    stream = client.chat.completions.create(
+        messages=[
+            {
+                "role":
+                "system",
+                "content":
+                "You act as an embedded development expert who helps me to build programs."
+            },
+            {
+                "role": "user",
+                "content": prompt
+            },
+        ],
+        model=model,
+        stream=True,
+    )
+    print(next(stream))
+    if stream:
+        for chunk in stream:
+            if chunk.choices[0].delta.content is not None:
+                print(chunk.choices[0].delta.content, end="", flush=True)
+    else:
+        print("Failed to request.")
+
+
+def parse_arguments():
+    # create object
+    parser = argparse.ArgumentParser(description='Process arguments.')
+    # add arguments
+    parser.add_argument(
+        '--model',
+        '-m',
+        type=str,
+        default="gpt-4-1106-preview",
+        choices=[
+            'gpt-3.5-turbo', 'gpt-3.5-1106', 'gpt-4', 'gpt-4-1106-preview',
+            'gpt-4-32k'
+        ],
+        help='The model name.',
+    )
+    parser.add_argument(
+        '--prompt',
+        '-p',
+        required=True,
+        type=str,
+        help="Input your prompt.",
+    )
+
+    #parse arguments
+    args = parser.parse_args()
+    return args
+
+
+def main():
+    args = parse_arguments()
+    request_gpt(args)
+
+
+if __name__ == "__main__":
+    main()
